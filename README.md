713aec9e5b0c933e6565ff1eccfa340d5aeaa971
jmsbell <74630856+jmsbell@users.noreply.github.com>
2024-01-20 13:04:46 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-qsiwcw
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jmsbell/web-platform-qsiwcw)
---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example: Change header text color on click
+const header = document.querySelector('header');
+header.addEventListener('click', () => {
+    header.style.color = '#ff4500';
+});
